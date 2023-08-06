b75497f955513915d5710c9b40c43e8f2aeada4a
Jose Antonio Verdugo <joseantonioverdugoardila@gmail.com>
2023-08-06 19:23:56 +0200

    starting frontend

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import "./bootstrap";
 
 import { createApp } from "vue";
-import App from "./components/App.vue";
+import Home from "./components/Home.vue";
 
-createApp(App).mount("#app");
+createApp(Home).mount("#app");
--- resources/js/components/prueba.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div>
-        <h1>componente de prueba</h1>
-    </div>
-</template>
-
-<script></script>
