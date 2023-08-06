46ad8a56dc747928171c8a9bad1b25a718b8b984
Jose Antonio Verdugo <joseantonioverdugoardila@gmail.com>
2023-08-06 12:39:51 +0200

    first commit

---
+++ resources/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+    </div>
+</template>
+
+<script setup>
+const title = "Todo with Laravel and Vue";
+</script>
+
+<style scoped>
+div {
+    text-align: center;
+    background-color: lightblue;
+}
+h1 {
+    color: lightcoral;
+}
+</style>
