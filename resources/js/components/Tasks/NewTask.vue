5f56bd033c31a9b838a6f85aa56dcb8f8e442f1c
Jose Antonio Verdugo <joseantonioverdugoardila@gmail.com>
2023-08-07 11:49:44 +0200

    DELETE & PUT working

--- resources/js/components/Tasks/NewTask.vue
+++ resources/js/components/Tasks/NewTask.vue
@@ -1,55 +1,3 @@
-<!-- <template>
-    <div>
-        <h1>Componente NewTask</h1>
-        <div>
-            <form @submit.prevent="createTask">
-                <div>
-                    <label for="task_name">Task Name</label>
-                    <input
-                        type="text"
-                        name="task_name"
-                        id="task_name"
-                        v-model="task.task_name"
-                    />
-                </div>
-                <div>
-                    <label for="task_description">Task Description</label>
-                    <input
-                        type="text"
-                        name="task_description"
-                        id="task_description"
-                        v-model="task.task_description"
-                    />
-                </div>
-                <div>
-                    <label for="task_status">Task Status</label>
-                    <input
-                        type="text"
-                        name="task_status"
-                        id="task_status"
-                        v-model="task.task_status"
-                    />
-                </div>
-                <div>
-                    <label for="task_category">Task Category</label>
-                    <input
-                        type="text"
-                        name="task_category"
-                        id="task_category"
-                        v-model="task.task_category"
-                    />
-                </div>
-                <div>
-                    <button type="submit">Create</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</template>
-
-<script>
--->
-
 <template>
     <div>
         <h2>Create New Task</h2>
@@ -102,19 +50,13 @@ export default {
             axios
                 .post("/api/tasks", newTask)
                 .then((response) => {
-                    // Handle the response from the API if needed
                     console.log("Task created:", response.data);
                     this.$router.push({ name: "home" });
                 })
                 .catch((error) => {
-                    // Handle errors if the API request fails
                     console.error("Error creating task:", error);
                 });
         },
     },
 };
 </script>
-
-<style scoped>
-/* Estilos específicos del componente aquí */
-</style>
